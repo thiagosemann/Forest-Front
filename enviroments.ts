337dfcb616b432300fc6e597d6d599af17e6a748
Thiago <42595307+thiagosemann@users.noreply.github.com>
2024-09-16 20:47:16 -0300

    Alterada a rota

--- enviroments.ts
+++ enviroments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    backendUrl: 'http://forest.kinghost.net:21055' // Substitua pela URL do seu backend
+    backendUrl: 'https://www.forest.app.br' // Substitua pela URL do seu backend
   };
   
