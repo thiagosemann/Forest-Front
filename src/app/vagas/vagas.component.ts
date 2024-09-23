a49150ee2a383b2b9ae4944681a9eba8bc77d6f6
Thiago <42595307+thiagosemann@users.noreply.github.com>
2024-09-23 17:22:03 -0300

    Terminado o componente Vagas

--- src/app/vagas/vagas.component.ts
+++ src/app/vagas/vagas.component.ts
@@ -6,7 +6,7 @@ import { Building } from '../shared/utilitarios/building';
 import { Apartamento } from '../shared/utilitarios/apartamento';
 import { ApartamentoService } from '../shared/service/Banco_de_Dados/apartamento_service';
 import { ToastrService } from 'ngx-toastr';
-import { FormBuilder, Validators, FormGroup, ValidatorFn, AbstractControl, FormControl } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-vagas',
@@ -16,31 +16,33 @@ import { FormBuilder, Validators, FormGroup, ValidatorFn, AbstractControl, FormC
 export class VagasComponent implements OnInit {
   vagas: Vaga[] = [];
   showEditComponent = false;
+  isEditing: boolean = false;  // Flag para indicar se está editando
   buildingId: number | null = null;
   buildings: Building[] = [];
   apartamentos: Apartamento[] = [];
-  myGroup: FormGroup; // Add a FormGroup property
-  registerForm: FormGroup; // FormGroup for the registration/editing of vaga
+  myGroup: FormGroup;
+  registerForm: FormGroup;
+  titleEditVaga: string = "Editar vaga";
 
-  constructor(private vagaService: VagaService,
-              private buildingService: BuildingService,
-              private apartamentoService: ApartamentoService,
-              private toastr: ToastrService,
-              private formBuilder: FormBuilder
+  constructor(
+    private vagaService: VagaService,
+    private buildingService: BuildingService,
+    private apartamentoService: ApartamentoService,
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder
+  ) {
+    this.myGroup = new FormGroup({
+      building_id: new FormControl(''),
+    });
 
-
-            ) {
-              this.myGroup = new FormGroup({
-                building_id: new FormControl(''), // Create a form control for 'building_id'
-              });
-              this.registerForm = this.formBuilder.group({
-                id: new FormControl(''), // Optional ID for editing
-                nome: new FormControl('', { validators: [Validators.required] }),
-                predio_id: new FormControl('', { validators: [Validators.required] }),
-                apartamento_id: new FormControl('', { validators: [Validators.required] }),
-                fracao: new FormControl('', { validators: [Validators.required] })
-              });
-            }
+    this.registerForm = this.formBuilder.group({
+      id: new FormControl(''), // ID opcional para edição
+      nome: new FormControl('', { validators: [Validators.required] }),
+      predio_id: new FormControl('Selecione', { validators: [Validators.required] }),
+      apartamento_id: new FormControl('Selecione', { validators: [Validators.required] }),
+      fracao: new FormControl('', { validators: [Validators.required] })
+    });
+  }
 
   ngOnInit(): void {
     this.getAllBuildings();
@@ -52,34 +54,51 @@ export class VagasComponent implements OnInit {
         this.buildings = buildings;
       },
       (error) => {
-        console.error('Error fetching buildings:', error);
+        console.error('Erro ao buscar prédios:', error);
       }
     );
   }
 
   onBuildingSelect(event: any): void {
     this.buildingId = event.target.value;
-    if(this.buildingId){
+    if (this.buildingId) {
       this.getAllApartamentosByBuildingId(this.buildingId);
     }
+  }
+
+  getAllApartamentosByBuildingId(buildingId: number): void {
+    this.apartamentoService.getApartamentosByBuildingId(buildingId).subscribe({
+      next: (apartamentos: Apartamento[]) => {
+        if (apartamentos.length == 0) {
+          this.toastr.error("Prédio sem apartamentos cadastrados!");
+        }
+        this.apartamentos = apartamentos;
+        this.getVagasByBuildingId(buildingId);
+      },
+      error: (error) => {
+        console.error('Erro ao buscar apartamentos:', error);
+      }
+    });
   }
 
   getVagasByBuildingId(buildingId: number): void {
     this.vagaService.getVagasByBuildingId(buildingId).subscribe({
       next: (vagas: Vaga[]) => {
         this.vagas = vagas;
-        if(vagas.length ==0){
-          this.toastr.error("Prédio sem vagas cadastradas!")
+        if (vagas.length == 0) {
+          this.toastr.error("Prédio sem vagas cadastradas!");
         }
       },
       error: (error) => {
-        console.error('Error fetching buildings:', error);
+        console.error('Erro ao buscar vagas:', error);
       }
     });
   }
 
   editVaga(vaga: Vaga): void {
     this.showEditComponent = true;
+    this.isEditing = true;
+    this.titleEditVaga = "Editar Vaga"
     this.registerForm.patchValue({
       id: vaga.id,
       nome: vaga.nome,
@@ -91,49 +110,76 @@ export class VagasComponent implements OnInit {
 
   cancelarEdit(): void {
     this.showEditComponent = false;
-    // Lógica para editar a vaga
+    this.isEditing = false;
+    this.registerForm.reset({
+      id: "",
+      nome: "",
+      predio_id: "Selecione",
+      apartamento_id: "Selecione",
+      fracao: ""
+    });
   }
 
   deleteVaga(vaga: Vaga): void {
-    if(vaga && vaga.id){
+    if (vaga && vaga.id) {
       if (confirm(`Você tem certeza que deseja excluir a vaga ${vaga.nome}?`)) {
         this.vagaService.deleteVaga(vaga.id).subscribe(() => {
+          this.toastr.success("Vaga deletada com sucesso!")
           this.getVagasByBuildingId(this.buildingId!);
         });
       }
     }
   }
 
-  getAllApartamentosByBuildingId(buildingId:number): void {
-    this.apartamentos=[];
-    this.apartamentoService.getApartamentosByBuildingId(buildingId).subscribe({
-      next: (apartamentos: Apartamento[]) => {
-        console.log(apartamentos);
-        if(apartamentos.length==0){
-          this.toastr.error("Prédio sem apartamentos cadastrados!")
-        }
-        this.apartamentos = apartamentos;
-        this.getVagasByBuildingId(buildingId);
-      
-      },
-      error: (error) => {
-        console.error('Error fetching buildings:', error);
-      }
+  addVaga(): void {
+    this.showEditComponent = true;
+    this.isEditing = false;  // Estamos criando uma nova vaga
+    this.titleEditVaga = "Criar Vaga"
+    this.registerForm.reset({  // Resetar o formulário para valores padrão
+      id: "",
+      nome: "",
+      predio_id: "Selecione",
+      apartamento_id: "Selecione",
+      fracao: ""
     });
   }
 
-  getAptNameById(vaga: Vaga):string{
-    let aptAux = this.apartamentos.find(apartamento=> apartamento.id == vaga.apartamento_id);
-    if(aptAux){
-      return aptAux.nome
-    }else{
-      return ""
+  onSubmit(): void {
+    if (this.registerForm.invalid) {
+      return;
     }
 
+    const vaga: Vaga = this.registerForm.value;
+    console.log(vaga)
+    if (this.isEditing) {
+      this.vagaService.updateVaga(vaga).subscribe({
+        next: () => {
+          this.toastr.success('Vaga atualizada com sucesso!');
+          this.showEditComponent = false;
+          this.getVagasByBuildingId(this.buildingId!);
+        },
+        error: (error) => {
+          this.toastr.error('Erro ao atualizar a vaga.');
+          console.error('Erro ao atualizar a vaga:', error);
+        }
+      });
+    } else {
+      this.vagaService.createVaga(vaga).subscribe({
+        next: () => {
+          this.toastr.success('Vaga criada com sucesso!');
+          this.showEditComponent = false;
+          this.getVagasByBuildingId(this.buildingId!);
+        },
+        error: (error) => {
+          this.toastr.error('Erro ao criar a vaga.');
+          console.error('Erro ao criar a vaga:', error);
+        }
+      });
+    }
   }
 
-  onSubmit():void{
-
+  getAptNameById(vaga: Vaga): string {
+    const aptAux = this.apartamentos.find(apartamento => apartamento.id == vaga.apartamento_id);
+    return aptAux ? aptAux.nome : "";
   }
-
 }
